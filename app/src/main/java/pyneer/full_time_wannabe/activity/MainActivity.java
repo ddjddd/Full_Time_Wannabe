865193c94491b88f05640d006370bd82f2edb826
jinho <ddjddd@naver.com>
2018-10-05 20:19:05 +0900

    Create project and initial setup Add Bindview, butterknife, and minor libraries

---
+++ app/src/main/java/pyneer/full_time_wannabe/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package pyneer.full_time_wannabe.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import pyneer.full_time_wannabe.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/pyneer/full_time_wannabe/utility/Constants.java
@@ -0,0 +1,8 @@
+package pyneer.full_time_wannabe.utility;
+
+import pyneer.full_time_wannabe.activity.MainActivity;
+
+public class Constants {
+    public static final Class[] ACTIVITES = {MainActivity.class};
+    public static final boolean DEBUG = true;
+}
