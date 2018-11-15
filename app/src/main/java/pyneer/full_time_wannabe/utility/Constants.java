7add33022c4097ed71cd687da034522df55145bd
jinho <ddjddd@naver.com>
2018-11-15 21:41:21 +0900

    Add Messenger

--- app/src/main/java/pyneer/full_time_wannabe/utility/Constants.java
+++ app/src/main/java/pyneer/full_time_wannabe/utility/Constants.java
@@ -1,12 +1,16 @@
 package pyneer.full_time_wannabe.utility;
 
+import java.lang.reflect.Member;
+
 import pyneer.full_time_wannabe.activity.MainActivity;
 import pyneer.full_time_wannabe.activity.main.LoginActivity;
+import pyneer.full_time_wannabe.activity.main.MessengerActivity;
 
 public class Constants {
     public static final Class[] ACTIVITES = {
             MainActivity.class,
-            LoginActivity.class
+            LoginActivity.class,
+            MessengerActivity.class
     };
     public static final boolean DEBUG = true;
 }
