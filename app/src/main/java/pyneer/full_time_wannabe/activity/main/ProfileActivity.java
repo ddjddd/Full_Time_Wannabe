392fce6b28d288bfed41eb3d6e8e84b9d724b107
ddjddd <ddjddd@naver.com>
2018-12-04 00:03:10 +0900

    Add profile activity

--- app/src/main/java/pyneer/full_time_wannabe/activity/main/ProfileActivity.java
+++ app/src/main/java/pyneer/full_time_wannabe/activity/main/ProfileActivity.java
@@ -2,28 +2,72 @@ package pyneer.full_time_wannabe.activity.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import pyneer.full_time_wannabe.R;
 import pyneer.full_time_wannabe.api.OnRestApiListener;
 import pyneer.full_time_wannabe.api.RestApiResult;
 
 public class ProfileActivity extends AppCompatActivity implements OnRestApiListener {
 
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.img_profile) ImageView img_profile;
+    @BindView(R.id.tv_name) TextView tv_name;
+    @BindView(R.id.tv_id) TextView tv_id;
+    @BindView(R.id.tv_phone) TextView tv_phone;
+    @BindView(R.id.lv_workplace) ListView lv_workplace;
+    @BindView(R.id.tv_account) TextView tv_account;
 
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiile);
+        ButterKnife.bind(this);
 
-
+        setToolBar();
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent (this, EditWorkPlaceActivity.class); // this로 쓰면 안 될 거 같은데...? : 쓰면 되네.
-        startActivity(intent);
+    private void setToolBar() {
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);        //커스터마이징 하기 위해 필요
+        actionBar.setDisplayHomeAsUpEnabled(true);          // 뒤로가기 버튼
     }
+//
+//    @OnClick(R.id.btn_logout)
+//    public void onBtnLogoutClick() {
+//
+//    }
+//
+//    @OnClick(R.id.btn_withdraw)
+//    public void onBtnWithdrawClick() {
+//
+//    }
+//
+//    @OnClick(R.id.btn_phone)
+//    public void onBtnPhoneClick() {
+//
+//    }
+//
+//    @OnClick(R.id.btn_workplace)
+//    public void onBtnWorkplaceClick() {
+//
+//    }
+//
+//    @OnClick(R.id.btn_account)
+//    public void onBtnAccountClick() {
+//
+//    }
 
 
     @Override
--- app/src/main/java/pyneer/full_time_wannabe/utility/Constants.java
+++ app/src/main/java/pyneer/full_time_wannabe/utility/Constants.java
@@ -1,10 +1,12 @@
 package pyneer.full_time_wannabe.utility;
 
 import pyneer.full_time_wannabe.activity.MainActivity;
+import pyneer.full_time_wannabe.activity.main.AttendanceActivity;
 import pyneer.full_time_wannabe.activity.main.ChatActivity;
 import pyneer.full_time_wannabe.activity.main.MessengerActivity;
 import pyneer.full_time_wannabe.activity.main.LoadingActivity;
 import pyneer.full_time_wannabe.activity.main.LoginActivity;
+import pyneer.full_time_wannabe.activity.main.ProfileActivity;
 import pyneer.full_time_wannabe.activity.main.StatisticsActivity;
 
 public class Constants {
@@ -12,10 +14,10 @@ public class Constants {
             MainActivity.class,
             LoginActivity.class,
             MessengerActivity.class,
-            LoginActivity.class,
             LoadingActivity.class,
             StatisticsActivity.class,
-            ChatActivity.class
+            AttendanceActivity.class,
+            ProfileActivity.class
     };
     public static final boolean DEBUG = true;
 }
